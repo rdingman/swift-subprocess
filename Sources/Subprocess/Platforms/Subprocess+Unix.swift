390f328243cc9075c421e2d4641e699172b3c8c5
Ryan Dingman <ryan@toyrockets.com>
2025-05-05 16:30:04 -0700

    WIP

--- Sources/Subprocess/Platforms/Subprocess+Unix.swift
+++ Sources/Subprocess/Platforms/Subprocess+Unix.swift
@@ -485,6 +485,12 @@ extension TrackedDispatchIO {
                     } else {
                         continuation.resume(returning: nil)
                     }
+                } else {
+                    if !buffer.isEmpty {
+                        continuation.resume(returning: SequenceOutput.Buffer(data: buffer))
+                    } else {
+                        continuation.resume(returning: nil)
+                    }
                 }
             }
         }
